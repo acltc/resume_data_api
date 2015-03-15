d4d29f90e0253ce4233c602e760fc8c14492e1e3
Shlomo Twerski <ystwerski@gmail.com>
2015-03-15 15:08:50 -0500

    fininshed controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,11 +2,13 @@ class StudentsController < ApplicationController
 
 	def show  
 		@student = Student.find(params[:id])
+		@experiences = @student.experiences
 	end
 
 	def update
 		@student = Student.find(params[:id])
-		@student.update([:first_name => params[:first_name], :last_name => params[:last_name], :email => params[:email], :phone_number => params[:phone_number], ])
+		@student.update({:first_name => params[:first_name], :last_name => params[:last_name], :email => params[:email], :phone_number => params[:phone_number], :linkedin => params[:linkedin], :twitter => params[:twitter], :blog => params[:blog], :online_resume => params[:online_resume], :github => params[:github], :photo => params[:photo], :short_bio => params[:short_bio]})
+		redirect_to student_path(params[:id])
 	end
 
 end
