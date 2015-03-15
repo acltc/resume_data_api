938721ea226d4e2639b93446cba9bdcbf9f320a9
Brian Vogelman <bvogelman7@gmail.com>
2015-03-15 15:01:12 -0500

    finished 2 controllers

--- app/controllers/educations_controller.rb
+++ app/controllers/educations_controller.rb
@@ -1,16 +1,16 @@
 class EducationsController < ApplicationController
 
-
-	def update
-
+def update
+		@education = Education.find(params[:id])
+		@education.update({:start_date => params[:start_date], :end_date => params[:end_Date], :degree => params[:degree], :university_name => params[:university_name]})
 	end
 
 	def create
-
+		@education = Education.create({:start_date => params[:start_date], :end_date => params[:end_Date], :degree => params[:degree], :university_name => params[:university_name], :student_id => params[:student_id]})
 	end
 
 	def delete
-
+		@education = Education.find(params[:id])
+		@education.destroy
 	end
-
 end
