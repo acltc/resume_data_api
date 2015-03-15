4f4c8e3f11095c25e02b7cbfd0d5eb0a6968eb66
brent morris <inkrypto@gmail.com>
2015-03-15 13:00:08 -0500

    merge conflict in controller resolved

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,7 @@
 class StudentsController < ApplicationController
 
-<<<<<<< HEAD
-  def show
-    @student = Student.find(.params[:id])
-=======
 	def show  
 		@student = Student.find(params[:id])
 	end
 
->>>>>>> 397df6e3192d0daf7f76d462ddcf044b816d2f9d
 end
