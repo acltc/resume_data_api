6fe7d9ad70b4d6897de21ac15f58dc775b504fa2
brent morris <inkrypto@gmail.com>
2015-03-15 15:06:42 -0500

    json adds

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,9 @@ class StudentsController < ApplicationController
 
 	def show  
 		@student = Student.find(params[:id])
+    @educations = @student.educations
+    @experiences = @student.experiences
+    @skills = @student.skills
 	end
 
 end
