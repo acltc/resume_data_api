0f2ce3ecfcd9e0030049f22e5f3f8ea9a848d6eb
Shlomo Twerski <ystwerski@gmail.com>
2015-03-16 12:27:25 -0500

    finished controllers and erased doubles from seed file. Still need to make views and figure out why associations do not work

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,18 @@
 class SkillsController < ApplicationController
+
+	def create 
+		@skill = Skill.create({:skill_name => params[:skill_name], :student_id => params[:student_id]})
+	end
+
+	def update
+		@skill = Skill.find(params[:id])
+		@skill.update({:skill_name => params[:skill_name]})
+	end
+
+	def delete
+		@skill = Skill.find(params[:id])
+		@skill.destroy
+	end
+
+
 end
