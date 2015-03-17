059f8864a3f722a4f7d387f22ebbc718bab0d69a
writeclubchicago1@yahoo.com <writeclubchicago1@yahoo.com>
2015-03-17 18:50:05 -0500

    ShowPageForExperiencesController

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,5 +1,9 @@
 class ExperiencesController < ApplicationController
 
+	def show  
+		@experience = Experience.find(params[:id])
+	end
+
 
 	def create
 		@experience = Experience.create({:start_date => params[:start_date], :end_date => params[:end_date], :job_title => params[:job_title], :company_name => params[:company_name], :student_id => params[:student_id]})
---
+++ app/views/experiences/show.json.jbuilder
@@ -0,0 +1,10 @@
+json.id @experience.id
+json.job_title @experience.job_title
+json.company_name @experience.company_name
+json.start_date @experience.start_date 
+json.end_date @experience.end_date
+json.details @experience.experience_details
+json.student_id @experience.student_id
+
+
+
