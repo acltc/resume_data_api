de73895b4d2ca766616c44dc44fd99e5a1c75aa2
brent morris <inkrypto@gmail.com>
2015-03-16 19:11:57 -0500

    Added Education_Details update and delete method

--- app/controllers/experience_details_controller.rb
+++ app/controllers/experience_details_controller.rb
@@ -2,18 +2,19 @@ class ExperienceDetailsController < ApplicationController
 
 	def create
 		@experience_detail = ExperienceDetail.create({:experience_id => params[:experience_id], :detail => params[:detail]})
-		render '/students/1.json'
+			render "students/show"
 	end
 
 	def update
 		@experience_detail = ExperienceDetail.find(params[:id])
-
 		@experience_detail.update({:experience_detail => params[:experience_detail]})
+			render "students/show"
 	end
 
 	def delete
 		@experience_detail = ExperienceDetail.find(params[:id])
 		@experience_detail.destroy
+			render "students/show"
 	end
 	
 end
