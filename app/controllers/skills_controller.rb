116ee70323d1066d2b9bc20a935303e1d2b56252
Shlomo Twerski <ystwerski@gmail.com>
2015-03-16 20:50:17 -0500

    Although have to test with postman, only thing needs to be done are the detail fields in views

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -2,16 +2,22 @@ class SkillsController < ApplicationController
 
 	def create 
 		@skill = Skill.create({:skill_name => params[:skill_name], :student_id => params[:student_id]})
+		@student = @skill.student
+		render "students/show"
 	end
 
 	def update
 		@skill = Skill.find(params[:id])
 		@skill.update({:skill_name => params[:skill_name]})
+		@student = @skill.student
+		render "students/show"
 	end
 
 	def delete
 		@skill = Skill.find(params[:id])
 		@skill.destroy
+		@student = @skill.student
+		render "students/show"
 	end
 
 
