c2a0b00e050bd1c8d61ec6fc3145b0224f31fa8d
Shlomo Twerski <ystwerski@gmail.com>
2015-03-16 20:03:17 -0500

    fixed experience_details controller

--- app/controllers/experience_details_controller.rb
+++ app/controllers/experience_details_controller.rb
@@ -2,18 +2,24 @@ class ExperienceDetailsController < ApplicationController
 
 	def create
 		@experience_detail = ExperienceDetail.create({:experience_id => params[:experience_id], :detail => params[:detail]})
-		render '/students/1.json'
+		@student = @experience_detail.experience.student
+		render "students/show"
 	end
 
 	def update
 		@experience_detail = ExperienceDetail.find(params[:id])
 
-		@experience_detail.update({:experience_detail => params[:experience_detail]})
+		@experience_detail.update({:detail => params[:experience_detail]})
+		p "that worked!!"
+		@student = @experience_detail.experience.student
+		render "students/show"
 	end
 
 	def delete
 		@experience_detail = ExperienceDetail.find(params[:id])
+		@student = @experience_detail.experience.student
 		@experience_detail.destroy
+		render "students/show"
 	end
 	
 end
