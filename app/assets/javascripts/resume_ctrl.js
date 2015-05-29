b84b762a4c01700e4ad4732c555ed4ea2a30c6aa
ystwerski <ystwerski@gmail.com>
2015-05-29 12:31:59 -0500

    Changed submit button to right on all the views. Added functionality for the education and references pages.

--- app/assets/javascripts/resume_ctrl.js
+++ app/assets/javascripts/resume_ctrl.js
@@ -7,7 +7,6 @@
   		var student = {first_name: firstName, last_name: lastName, email: email, phone_number: phoneNumber, github: github, blog: blog, twitter: twitter, linkedin: linkedin};
   		$http.post("/api/v1/students.json", student).then(function(response){
   			$scope.students.push(student);
-
   		}), function(error){
   			$scope.errors = error.data.errors;
   		}
@@ -21,6 +20,24 @@
   			$scope.errors = error.data.errors;
   		}
   	}
+
+    $scope.addNewEducation = function(degreeEarned, universityName, startDate, endDate){
+      var education = {company_name: companyName, job_title: jobTitle, start_date: startDate, end_date: endDate};
+      $http.post("/api/v1/educations.json", education).then(function(response){
+        $scope.educations.push(education);
+      }), function(error){
+        $scope.errors = error.data.errors;
+      }
+    }
+
+    $scope.addNewReference = function(firstName, lastName, email, phoneNumber, companyName){
+      var education = {company_name: companyName, job_title: jobTitle, start_date: startDate, end_date: endDate};
+      $http.post("/api/v1/educations.json", education).then(function(response){
+        $scope.educations.push(education);
+      }), function(error){
+        $scope.errors = error.data.errors;
+      }
+    }
     
     window.scope = $scope;
 
