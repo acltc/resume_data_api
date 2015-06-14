9060faf2a26b489ad5f0935c740b41fd547a430e
ystwerski <ystwerski@gmail.com>
2015-06-14 17:02:31 -0500

    fixed paths on views. deleted extra headers in all the views because everything is in application.html. Put logo in application.html and deleted home from the navbar.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :students
       resources :experiences
+      resources :educations
+      resources :proffessional_skills
+      resources :personal_skills
+      resources :references
     end
   end
   resources :personal_informaions
