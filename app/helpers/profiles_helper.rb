fb39b463b2ba449e442dbb2f1f8e0a0e25ab78ec
Troy Leach <troyleach29@gmail.com>
2015-06-21 13:35:28 -0500

    progress on error pages

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -3,8 +3,11 @@ module ProfilesHelper
   def users_name(name)
     
     @students.each do |full_name|
-      if name == full_name.first_name.downcase + full_name.last_name.downcase
+      user_name = full_name.first_name.downcase + full_name.last_name.downcase
+      if user_name == name
         return full_name.first_name
+      else
+                
       end
     end
 
